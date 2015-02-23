5f2a835f892b76b976dc26e73958ea209beb6aa2
Bret Comnes <bcomnes@gmail.com>
2015-02-23 14:49:10 -0800

    Deep mixinto options

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
+var deepMixIn = require('mout/object/deepMixIn')
 var standardPreset = require('./node_modules/standard-format/rc/esformatter.json')
 
 exports.standardPreset = standardPreset
 
-exports.setOptions = function(opts) {
-  opts = standardPreset
+exports.setOptions = function (opts) {
+  deepMixIn(opts, standardPreset)
 }
